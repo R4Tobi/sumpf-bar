9a80dfc8cda9b2b5eeb17d6a6f8e645e568456e6
Tobias Baake <62656419+R4Tobi@users.noreply.github.com>
2025-12-04 22:26:09 +0100

    updated App Component

--- vue/src/App.vue
+++ vue/src/App.vue
@@ -1,11 +1,8 @@
-<script setup lang="ts"></script>
-
+<!-- eslint-disable @typescript-eslint/no-unused-vars -->
+<!-- eslint-disable vue/no-deprecated-slot-attribute -->
 <template>
-  <h1>You did it!</h1>
-  <p>
-    Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
-    documentation
-  </p>
+  <router-view></router-view>
 </template>
 
-<style scoped></style>
+<style>
+</style>
