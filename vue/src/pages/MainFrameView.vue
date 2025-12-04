12e1b43d6717bf3ffc2a8297ea37502495d5e030
Tobias Baake <62656419+R4Tobi@users.noreply.github.com>
2025-12-04 22:24:47 +0100

    added MainFrameView

---
+++ vue/src/pages/MainFrameView.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+import ArticleLightComponent from '@/components/ArticleLightComponent.vue';
+import LandingPageComponent from '@/components/LandingPageComponent.vue';
+
+</script>
+
+<template>
+    <LandingPageComponent />
+    <ArticleLightComponent />
+</template>
